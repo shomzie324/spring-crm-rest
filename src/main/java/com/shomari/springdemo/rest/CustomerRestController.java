24579903a8c86909ee52d76f199cb6f4d427bc47
Shomari Malcolm <shomarimalcolm@gmail.com>
2019-04-24 16:51:32 -0400

    Added update and delete customer end points with error handling

--- src/main/java/com/shomari/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/shomari/springdemo/rest/CustomerRestController.java
@@ -5,8 +5,12 @@ import com.shomari.springdemo.entity.Customer;
 import com.shomari.springdemo.service.CustomerService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +39,52 @@ public class CustomerRestController {
     }
 
     return customer;
+  }
+
+  // add post mapping for /customers
+  @PostMapping("/customers")
+  public Customer addCustomer(@RequestBody Customer theCustomer){
+
+    // set id to 0 to ensure hibernate treats it as new record for db
+    theCustomer.setId(0);
+
+    // hibernate will auto set id of customer object to reflect the actual id that it was given
+    // in the db
+    customerService.saveCustomer(theCustomer);
+
+    return theCustomer;
+  }
+
+  // add mapping four put /customers/customerId - update an existing customer
+  @PutMapping("/customers")
+  public Customer updateCustomer(@RequestBody Customer theCustomer){
+    int id = theCustomer.getId();
+    Customer tempCustomer =  customerService.getCustomer(theCustomer.getId());
+
+    // throw exception if customer is null - nothing returned from db
+    if(tempCustomer ==  null){
+      throw new CustomerNotFoundException("Customer id not found: - " + id);
+    }
+
+    customerService.saveCustomer(theCustomer);
+
+    return theCustomer;
+  }
+
+  // add delete mapping for /customers/customerId
+  @DeleteMapping("/customers/{customerId}")
+  public String deleteCustomer(@PathVariable int customerId){
+
+    Customer tempCustomer =  customerService.getCustomer(customerId);
+
+    // throw exception if customer is null - nothing returned from db
+    if(tempCustomer ==  null){
+      throw new CustomerNotFoundException("Customer id not found: - " + customerId);
+    }
+
+    customerService.deleteCustomer(customerId);
+
+    return "Deleted customer id - " + customerId;
   }
 
 }
