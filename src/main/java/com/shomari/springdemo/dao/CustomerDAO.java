9d61017d2f5acc4f3b9d9eaa42efd7a7834d08ef
Shomari Malcolm <shomarimalcolm@gmail.com>
2019-04-24 15:18:55 -0400

    completed initial set up of DAO and customer service for interacting with mySQL database

---
+++ src/main/java/com/shomari/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.shomari.springdemo.dao;
+
+import java.util.List;
+
+import com.shomari.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
